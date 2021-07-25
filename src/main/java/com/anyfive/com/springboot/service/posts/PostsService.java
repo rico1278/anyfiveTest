ff2060f0444049a4fcd7e150587697869670e8bc
gituser <narchisel@naver.com>
2021-07-25 21:03:08 +0900

    message

--- src/main/java/com/anyfive/com/springboot/service/posts/PostsService.java
+++ src/main/java/com/anyfive/com/springboot/service/posts/PostsService.java
@@ -2,13 +2,17 @@ package com.anyfive.com.springboot.service.posts;
 
 import com.anyfive.com.springboot.domain.posts.Posts;
 import com.anyfive.com.springboot.domain.posts.PostsRepository;
+import com.anyfive.com.springboot.web.dto.PostsListResponseDto;
 import com.anyfive.com.springboot.web.dto.PostsResponseDto;
 import com.anyfive.com.springboot.web.dto.PostsSaveRequestsDto;
 import com.anyfive.com.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -31,5 +35,12 @@ public class PostsService {
         Posts entity = postsRepository.findById(id)
                                     .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
