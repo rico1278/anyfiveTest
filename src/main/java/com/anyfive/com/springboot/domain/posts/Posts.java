15d8c594812b28c3c3318e380fe94b3a8e804c4a
gituser <narchisel@naver.com>
2021-07-25 10:59:44 +0900

    message

--- src/main/java/com/anyfive/com/springboot/domain/posts/Posts.java
+++ src/main/java/com/anyfive/com/springboot/domain/posts/Posts.java
@@ -1,4 +1,42 @@
 package com.anyfive.com.springboot.domain.posts;
 
-public class Posts {
+import com.anyfive.com.springboot.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@DynamicUpdate
+@DynamicInsert
+public class Posts extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public void update(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
 }
---
+++ src/main/java/com/anyfive/com/springboot/web/PostsApiContoller.java
@@ -0,0 +1,30 @@
+package com.anyfive.com.springboot.web;
+
+import com.anyfive.com.springboot.service.posts.PostsService;
+import com.anyfive.com.springboot.web.dto.PostsResponseDto;
+import com.anyfive.com.springboot.web.dto.PostsSaveRequestsDto;
+import com.anyfive.com.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiContoller {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestsDto requestsDto){
+        return postsService.save(requestsDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestsDto){
+        return postsService.update(id, requestsDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id){
+        return postsService.findById(id);
+    }
+}
